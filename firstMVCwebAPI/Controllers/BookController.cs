fa3be9a58d2b8ba0f00fc08ff37fb0851cbee6e6
aymen21061999 <mohmadaymen.naqash@gavstech.com>
2023-09-21 14:07:42 +0530

    update

---
+++ firstMVCwebAPI/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using firstMVCwebAPI.Models;
+namespace firstMVCwebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        static List<Book> booklist = new List<Book>();
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Book book = new Book();
+                book.BookID = i;
+                book.Title = "Atomic Habits" + i;
+                book.Cost = i + 100;
+                book.AuthorName = "Author" + i;
+                booklist.Add(book);
+            }
+            return booklist;
+        }
+        [HttpPost]
+        public int AddBook(Book book)
+        {
+            booklist.Add(book);
+            return 1;
+        }
+    }
+}
---
+++ firstMVCwebAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace firstMVCwebAPI.Models
+{
+    public class Book
+    {
+        public int BookID { set; get; }
+        public String Title { set; get; } = String.Empty;
+        public float Cost { set; get; }
+        public String AuthorName { set; get; } = String.Empty;
+    }
+}
